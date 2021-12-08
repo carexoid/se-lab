b16e5a8204afa121ddf77f7b6bfa990c5938d59d
Daniil <you@example.com>
2021-12-08 23:05:19 +0200

    Add ban list to admin

--- back/admin/views.py
+++ back/admin/views.py
@@ -1,5 +1,7 @@
 from flask_admin.contrib.peewee import ModelView, filters
 from wtforms import fields
+from wtfpeewee.orm import model_form
+from peewee import fn
 from back.chief import db
 
 
@@ -10,6 +12,45 @@ class BaseModelView(ModelView):
 class UserView(BaseModelView):
     can_create = False
     column_filters = ("id", "auth_id")
+
+    @staticmethod
+    def _user_has_access_impl(model):
+        return db.BannedUser.select(fn.COUNT(db.BannedUser.id))\
+            .where(db.BannedUser.id == model.id).scalar() == 0
+
+    def _user_has_access(self, context, model, name):
+        return UserView._user_has_access_impl(model)
+
+    column_formatters = {
+        'has_access': _user_has_access
+    }
+
+    def get_column_names(self, only_columns, excluded_columns):
+        only_columns.append('has_access')
+        return super().get_column_names(only_columns, excluded_columns)
+
+    form_extra_fields = {
+        "has_access": fields.BooleanField("Has access")
+    }
+
+    def after_model_change(self, form, model, is_created):
+        if not is_created:
+            has_access = UserView._user_has_access_impl(model)
+            will_have_access = form.data["has_access"]
+            print(will_have_access)
+
+            if has_access and not will_have_access:
+                db.BannedUser.create(id=model.id)
+
+            if not has_access and will_have_access:
+                print(model.id)
+                db.BannedUser.delete().where(db.BannedUser.id == model.id).execute()
+
+        return super().after_model_change(form, model, is_created)
+
+    def on_model_delete(self, model):
+        db.BannedUser.delete().where(db.BannedUser.id == model.id).execute()
+        return super().on_model_delete(model)
 
 
 class AirportView(BaseModelView):
@@ -57,7 +98,7 @@ class FlightView(BaseModelView):
             FlightView.create_tickets(form, model)
 
     def on_model_delete(self, model):
-        db.Ticket.delete().where(db.Ticket.flight == model.id)
+        db.Ticket.delete().where(db.Ticket.flight == model.id).execute()
         return super().on_model_delete(model)
 
 
@@ -66,13 +107,6 @@ class OrderView(BaseModelView):
     column_filters = ("id", filters.FilterEqual(
         column=db.Order.user, name="User"))
     form_excluded_columns = ("user", "created_at")
-
-    form_choises = {
-        "state": [
-            (0, "active"),
-            (1, "completed")
-        ]
-    }
 
     def on_model_change(self, form, model, is_created):
         model.state = db.Order.State(form.data["state"])
