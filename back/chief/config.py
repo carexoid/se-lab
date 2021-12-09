31eef1bd6249daa8cf6b9ce7c92a5524fa3cda19
starquell <ihayduchenko@gmail.com>
2021-12-09 23:35:50 +0200

    delete user works

--- back/chief/config.py
+++ back/chief/config.py
@@ -1,3 +1,4 @@
 DATABASE = 'mysql+pool://chief:ChiefDevP@$$@134.209.225.223:3306/airport'
 PAYMENT_SERVICE = "http://localhost:5001"
 GOTRUE_URL = "http://shlyahdomrii.tech/.netlify/identity"
+GOTRUE_ADMIN_KEY = r"'$[}L7Vk#B>v$f=\<dGtRF^Nb4}E9Yv{)rX"
